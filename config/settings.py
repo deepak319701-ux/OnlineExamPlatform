5246fabdcfb0add21b04bd632df6f02ce31527fa
Deepak Kumar <deepak319701@gmail.com>
2026-07-11 02:19:42 +0530

    Production settings for Render

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,14 +20,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^d29wy#b2go=mut#so0i$m45h97&dm#y2tg4k#h_8*6#-dqib^'
-
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-^d29wy#b2go=mut#so0i$m45h97&dm#y2tg4k#h_8*6#-dqib^",
+)
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
+    ".onrender.com",
 ]
 
 
